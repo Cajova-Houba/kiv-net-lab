5d5cdeaef3316d675c36c98e3ffe0b6af930b9fa
Cajova-Houba <tkmushroomer@gmail.com>
2018-02-26 09:31:57 +0100

    Lab 2 added.

---
+++ l2/NetLab2/Invoice/Invoice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Invoice
+{
+    public class Invoice
+    {
+        private String invoiceNumber;
+        private DateTime dateInvoice;
+        private DateTime dueDate;
+
+        public String InvoiceNumber { get { return invoiceNumber; } set { invoiceNumber = value; } }
+        public DateTime DateInvoice { get { return dateInvoice; } set { dateInvoice = value; } }
+        public DateTime DueDate { get { return dueDate; } set { dueDate = value; } }
+        public Boolean IsOverDue
+        {
+            get
+            {
+                return dueDate < DateTime.Now;
+            }
+        }
+
+        public Invoice()
+        {
+        }
+
+        public Invoice(string invoiceNumber, DateTime dateInvoice, DateTime dueDate)
+        {
+            this.invoiceNumber = invoiceNumber;
+            this.dateInvoice = dateInvoice;
+            this.dueDate = dueDate;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\t{1}\t{2}{3}", InvoiceNumber, DateInvoice, DueDate, IsOverDue ? "\t!!!" : "");
+        }
+
+    }
+}
---
+++ l2/NetLab2/NetLab2/InvoiceApp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLab2
+{
+    class InvoiceApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Invoice App");
+            Console.Out.WriteLine("Generating invoices, press any key to stop...");
+            Console.Out.WriteLine("Number\tDate\tDue date\tOver due");
+            Random r = new Random();
+            while(!Console.KeyAvailable)
+            {
+                int invNum = r.Next(8999) + 1000;
+                int invDateRange = r.Next(5) - 10;
+                int dueDateRange = r.Next(5) - 3;
+                Invoice.Invoice inv = new Invoice.Invoice(invNum.ToString(), DateTime.Now.AddDays(invDateRange), DateTime.Now.AddDays(dueDateRange));
+                Console.WriteLine(inv);
+                Thread.Sleep(1000);
+            }
+            Console.ReadKey();
+
+            Console.Out.WriteLine("Done. Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
