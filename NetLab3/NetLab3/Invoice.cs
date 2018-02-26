2b50d667ac39dda9e2d5c1eaf68a87b477d8ee55
Cajova-Houba <tkmushroomer@gmail.com>
2018-02-26 10:03:52 +0100

    Lab 3 added.

---
+++ NetLab3/NetLab3/Invoice.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLab3
+{
+
+    /// <summary>
+    /// Units used in invoice.
+    /// </summary>
+    public enum InvoiceUnits
+    {
+        /// <summary>
+        /// 1 item.
+        /// </summary>
+        ITEM,
+
+        /// <summary>
+        /// 1 kg.
+        /// </summary>
+        KG,
+
+        /// <summary>
+        /// 1 liter.
+        /// </summary>
+        L
+
+    }
+
+    /// <summary>
+    /// Structure which represents one item billed in invoice.
+    /// </summary>
+    public struct InvoiceItem
+    {
+        private String name;
+        private InvoiceUnits unit;
+        private double pricePerUnit;
+        private int unitCount;
+
+        /// <summary>
+        /// Gets or sets the name of this item.
+        /// </summary>
+        public string Name { get => name; set => name = value; }
+
+        /// <summary>
+        /// Gets or sets unit of this item.
+        /// </summary>
+        public InvoiceUnits Unit { get => unit; set => unit = value; }
+
+        /// <summary>
+        /// Gets or sets price per one unit of this item.
+        /// </summary>
+        public double PricePerUnit { get => pricePerUnit; set => pricePerUnit = value; }
+
+        /// <summary>
+        /// Gets or sets unit count of this item. 
+        /// </summary>
+        public int UnitCount { get => unitCount; set => unitCount = value; }
+
+        /// <summary>
+        /// Returns total price without DPH.
+        /// </summary>
+        public double TotalPrice { get => unitCount * pricePerUnit; }
+
+        /// <summary>
+        /// Return total price with DPH included.
+        /// </summary>
+        public double TotalPriceDPH { get => 1.21 * TotalPrice; }
+
+        /// <summary>
+        /// Returns DPH for this item.
+        /// </summary>
+        public double DPH { get => 0.21 * TotalPrice; }
+    
+        public InvoiceItem(string name, InvoiceUnits unit, double pricePerUnit, int unitCount)
+        {
+            this.name = name;
+            this.unit = unit;
+            this.pricePerUnit = pricePerUnit;
+            this.unitCount = unitCount;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\t{1}\t{2:0.##}\t{3}\t{4:0.##}\t{5:0.####}\t{6:0.####}", Name, Unit, PricePerUnit, UnitCount, DPH, TotalPrice, TotalPriceDPH);
+        }
+    }
+}
---
+++ NetLab3/NetLab3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generating invoice items...");
+            LinkedList<InvoiceItem> invoiceItems = new LinkedList<InvoiceItem>();
+            Random r = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                double rDouble = r.NextDouble();
+                InvoiceUnits unit = rDouble < 0.3 ? InvoiceUnits.ITEM : (rDouble < 0.6 ? InvoiceUnits.KG : InvoiceUnits.L);
+                InvoiceItem item = new InvoiceItem("Item " + i, unit, r.NextDouble() * (r.Next(100) + 1), r.Next(1000) + 1);
+                Console.WriteLine(item);
+                invoiceItems.AddLast(item);
+            }
+            Console.WriteLine("Done.");
+
+            var totalPriceDPHQuery =
+                from item in invoiceItems
+                select item.TotalPriceDPH;
+
+            var totalPriceQuery =
+                from item in invoiceItems
+                select item.TotalPrice;
+
+            Console.WriteLine(String.Format("Total price (excluding DPH): {0:0.####} Kč", totalPriceQuery.Sum()));
+            Console.WriteLine(String.Format("Total price (including DPH): {0:0.####} Kč", totalPriceDPHQuery.Sum()));
+            Console.Read();
+        }
+    }
+}
